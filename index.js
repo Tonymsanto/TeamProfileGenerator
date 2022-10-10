13d069ac403a150802fcfae1b4af7ae640c670ee
Anthony Santo <tonymsanto@gmail.com>
2022-10-09 18:32:29 -0600

    Updated JS files under lib folder

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 
-const makeHTML = require('./src/makeHTML')
-const employee = require('./lib/employee')
-const engineer = require('./lib/engineer')
-const intern = require ('./lib/intern')
-const manager = require ('./lib/manager')
+const makeHTML = require('./src/makeHTML');
+const employee = require('./lib/employee');
+const engineer = require('./lib/engineer');
+const intern = require ('./lib/intern');
+const manager = require ('./lib/manager');
+const { type } = require('os');
+
+
+//Prompts for the manager
+inquirer.prompt([
+    {
+     message: 'What is the name of the manager?',
+     type: 'input',
+     name: 'name'
+    },
+    {
+        message: 'What is the id of the manager?',
+        type: 'input',
+        name: 'id'
+    },
+    {
+        message: 'What is the manager\'s email?',
+        type: 'input',
+        name: 'email'
+    },
+    {
+        message: 'What is the manager\'s officenumber?',
+        type: 'input',
+        name: 'officeNumber'
+    }
+]);
